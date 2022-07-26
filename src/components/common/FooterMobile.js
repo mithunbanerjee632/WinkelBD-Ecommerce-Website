c5317ce79da9cbd1604a1c91bea117d30360e48c
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-07-26 21:37:22 +0600

    Modified Navmenu mobile component

--- src/components/common/FooterMobile.js
+++ src/components/common/FooterMobile.js
@@ -1,5 +1,6 @@
 import React, {Component, Fragment} from 'react';
 import {Col, Container, Row} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 class FooterMobile extends Component {
     render() {
@@ -12,7 +13,12 @@ class FooterMobile extends Component {
                                 <h5 className="footer-menu-title">OFFICE ADDRESS</h5>
                                 <p>West Dholairpar,Shyampur,Dhaka-1204.      Help Line: 01713574869, deshibazar@gmail.com</p>
 
-                                <h5 className="footer-menu-title">SOCIAL LINK</h5>
+                                <h5 className="footer-menu-title">More Info</h5>
+                                <Link to="/purchase" className="footer-link">How To Purchase</Link><br/>
+                                <Link to="/policy" className="footer-link">Privacy Policy</Link><br/>
+                                <Link  to="/refund" className="footer-link">Refund Policy</Link><br/>
+
+                                <h5 className="footer-menu-title mt-2">SOCIAL LINK</h5>
                                 <a target="_blank" href=""><i className="fab h4 fa-facebook"/> </a>
                                 <a target="_blank" href=""><i className="fab mx-1 h4 fa-instagram"/> </a>
                                 <a target="_blank" href=""><i className="fab  h4 fa-twitter"/></a>
--- src/components/common/NavMenuMobile.js
+++ src/components/common/NavMenuMobile.js
@@ -3,6 +3,10 @@ import {Button, Col, Container, Navbar, Row} from "react-bootstrap";
 import MegaMenu from "../home/MegaMenu";
 import MegaMenuMobile from "../home/MegaMenuMobile";
 import logo from "../../assets/images/logo.png";
+import {Link} from "react-router-dom";
+import notification from "../../assets/images/notification.svg";
+import love from "../../assets/images/love.svg";
+import user from "../../assets/images/user.svg";
 
 class NavMenuMobile extends Component {
     constructor(props) {
@@ -40,12 +44,20 @@ class NavMenuMobile extends Component {
                     <Row>
                         <Col className="p-1 " lg={12} md={12} sm={12} xs={12} >
                             <Button onClick={this.MenuBarClickHandler} className="btn"><i className="fa fa-bars"></i></Button>
-                            <a href="" className="btn"><img className="nav-logo" src={logo} alt="logo"/></a>
-                                <Button className="cart-btn"><i className="fa fa-shopping-cart"></i>4 items</Button>
+
+                                {/*<Button className="cart-btn"><i className="fa fa-shopping-cart"></i>4 items</Button>*/}
+
+
+                            <Link to="/cart" className="cart-btn float-right"><i className="fa fa-shopping-cart"/> 4 items </Link>
+                            <Link to="/favourite" className="btn mx-1 nav-round-btn"><i className="fa fa-search"/> </Link>
+                            <Link to="/notification" className="btn mx-1 nav-round-btn"><img src={notification} alt="notification"/></Link>
+                            <Link to="/onboard" className=" btn-light btn"><img src={user} alt="user"/></Link>
+
                         </Col>
                     </Row>
 
                     <div className={this.state.SideNavState}>
+                        <a href="" className="btn"><img className="nav-logo" src={logo} alt="logo"/></a>
                         <MegaMenuMobile/>
                     </div>
 
