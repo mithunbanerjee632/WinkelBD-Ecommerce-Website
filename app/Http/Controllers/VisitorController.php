31c342be10dfdf91a7092fbe1daad2ae979cdf36
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-07-30 20:32:23 +0600

    Rest API for Visitors

---
+++ app/Http/Controllers/VisitorController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\VisitorModel;
+use Illuminate\Http\Request;
+
+class VisitorController extends Controller
+{
+    function VisitorDetails(){
+        $ip_address = $_SERVER['REMOTE_ADDR'];
+        date_default_timezone_set('Asia/Dhaka');
+        $visit_time =date('h:i:sa');
+        $visit_date =date('d-m-Y');
+
+        $result = VisitorModel::insert([
+            'ip_address'=>$ip_address,
+            'visit_time'=>$visit_time,
+            'visit_date'=>$visit_date
+        ]);
+
+        return $result;
+    }
+}
