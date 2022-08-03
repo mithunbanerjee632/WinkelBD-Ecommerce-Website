2956da42de93ccf10121fbbb9de5241c74788fe2
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-08-03 12:48:23 +0600

    Consume How To Purchase API from Client side

--- src/components/others/Purchase.js
+++ src/components/others/Purchase.js
@@ -10,34 +10,58 @@ import ReactHtmlParser from "react-html-parser";
 class Purchase extends Component {
     constructor() {
         super();
+        this.state={
+            purchase:"",
+            isLoading:true,
+            isError:false
+        }
+    }
+
+    componentDidMount() {
+        if(sessionStorage.getItem('siteInfoPurchase')==null){
+            Axios.get(AppUrl.SiteInfoDetails).then(response=>{
+                if(response.status==200){
+                    let jsonData = (response.data)[0]['purchase_guide'];
+                    this.setState({purchase:jsonData,isLoading:false,isError:false})
+                    sessionStorage.setItem('siteInfoPurchase',jsonData);
+                }else{
+                    this.setState({isLoading:false,isError:true});
+                }
+            }).catch(error=>{
+                this.setState({isLoading:false,isError:true});
+            });
+        }else{
+            this.setState({purchase:sessionStorage.getItem('siteInfoPurchase'),isLoading:false,isError:false});
+        }
 
     }
+
     render() {
-        return (
-            <Fragment>
-                <Container className="TopSection bg-white">
-                    <Row>
-                        <Col className="mt-1" md={12} lg={12} sm={12} xs={12}>
+        if(this.state.isLoading==true && this.state.isError==false){
+            return <Loading/>;
+        }else if(this.state.isLoading==false && this.state.isError==false){
+            return (
+                <Fragment>
+                    <Container className="TopSection bg-white">
+                        <Row>
+                            <Col className="mt-1" md={12} lg={12} sm={12} xs={12}>
 
-                                    <div className="">
-                                        <div className="animated zoomIn">
-                                            <p>Deshi Bazar is New Ecommerce Company in Bangladesh.We want supply authentic product to our valuable custormers.</p>
-                                            <p>Deshi Bazar is New Ecommerce Company in Bangladesh.We want supply authentic product to our valuable custormers.</p>
-                                            <p>Deshi Bazar is New Ecommerce Company in Bangladesh.We want supply authentic product to our valuable custormers.</p>
-                                            <p>Deshi Bazar is New Ecommerce Company in Bangladesh.We want supply authentic product to our valuable custormers.</p>
-                                            <p>Deshi Bazar is New Ecommerce Company in Bangladesh.We want supply authentic product to our valuable custormers.</p>
-                                            <p>Deshi Bazar is New Ecommerce Company in Bangladesh.We want supply authentic product to our valuable custormers.</p>
-                                            <p>Deshi Bazar is New Ecommerce Company in Bangladesh.We want supply authentic product to our valuable custormers.</p>
+                                <div className="p-2">
+                                    <div className="animated zoomIn">
+                                        {ReactHtmlParser(this.state.purchase)}
 
-
-                                        </div>
                                     </div>
+                                </div>
 
-                        </Col>
-                    </Row>
-                </Container>
-            </Fragment>
-        );
+                            </Col>
+                        </Row>
+                    </Container>
+                </Fragment>
+            );
+        }else if(this.state.isLoading==false && this.state.isError==true){
+            return <WentWrong/>
+        }
+
     }
 }
 
