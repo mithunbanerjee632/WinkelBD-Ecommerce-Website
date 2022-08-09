fa64a4e5edad57a70c6900d32d5e7a34f9d3001e
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-08-09 12:05:58 +0600

    consume category and sub category API from Client Side

--- src/components/home/Categories.js
+++ src/components/home/Categories.js
@@ -1,187 +1,54 @@
 import React, {Component, Fragment} from 'react';
 import {Card, Col, Container, Row} from "react-bootstrap";
-
+import Axios from "axios";
+import AppUrl from "../../api/AppUrl";
 class Categories extends Component {
+    constructor() {
+        super();
+        this.state={
+            categories:[]
+        }
+    }
+
+    componentDidMount() {
+        Axios.get(AppUrl.CategoryDetails).then(response=>{
+            if(response.status==200){
+                let jsonData = response.data;
+                this.setState({categories:jsonData});
+            }
+        }).catch(error=>{
+
+        });
+    }
+
     render() {
+
+        let myLists = this.state.categories;
+
+        let myView = myLists.map((Categories,i)=>{
+            return  <Col key={i.toString()} className="p-0"  xl={2} lg={2} md={2} sm={6} xs={6}>
+
+                <Card className="h-100 w-100 text-center">
+
+                    <Card.Body>
+                        <img className="w-75" src={Categories.ParentCategoryImage}/>
+                        <h4 className="category-name">{Categories.ParentCategoryName}</h4>
+                    </Card.Body>
+                </Card>
+
+            </Col>
+        });
         return (
             <Fragment>
                 <Container fluid={true} className="text-center BetweenTwoSection">
                     <h4 className="section-title">CATEGORIES</h4>
-                    <p className="section-sub-title">Some of Our Exclusive Collection.You May Like</p>
+                    {/*<p className="section-sub-title">Some of Our Exclusive Collection.You May Like</p>*/}
                     <Row>
-
-                        <Col key={1} xl={6} lg={6} md={6} sm={12} xs={12}>
-                            <Row>
-                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                                    <Card className="h-100 w-100 text-center">
-
-                                        <Card.Body>
-                                            <img className="w-75" src="https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/catalog/product/cache/a4a71470c99ce7a4925275fbc94e38e5/a/m/amazfit-gts-4-mini-smart-watch-17-07-2022.jpg"/>
-                                            <h4 className="category-name">CATEGORY NAME</h4>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-
-                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                                    <Card className="h-100 w-100 text-center">
-
-                                        <Card.Body>
-                                            <img className="w-75" src="https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/catalog/product/cache/a4a71470c99ce7a4925275fbc94e38e5/a/m/amazfit-gts-4-mini-smart-watch-17-07-2022.jpg"/>
-                                            <h4 className="category-name">CATEGORY NAME</h4>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-
-                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                                    <Card className="h-100 w-100 text-center">
-
-                                        <Card.Body>
-                                            <img className="w-75" src="https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/catalog/product/cache/a4a71470c99ce7a4925275fbc94e38e5/a/m/amazfit-gts-4-mini-smart-watch-17-07-2022.jpg"/>
-                                            <h4 className="category-name">CATEGORY NAME</h4>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-
-                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                                    <Card className="h-100 w-100 text-center">
-
-                                        <Card.Body>
-                                            <img className="w-75" src="https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/catalog/product/cache/a4a71470c99ce7a4925275fbc94e38e5/a/m/amazfit-gts-4-mini-smart-watch-17-07-2022.jpg"/>
-                                            <h4 className="category-name">CATEGORY NAME</h4>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-
-                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                                    <Card className="h-100 w-100 text-center">
-
-                                        <Card.Body>
-                                            <img className="w-75" src="https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/catalog/product/cache/a4a71470c99ce7a4925275fbc94e38e5/a/m/amazfit-gts-4-mini-smart-watch-17-07-2022.jpg"/>
-                                            <h4 className="category-name">CATEGORY NAME</h4>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-
-                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                                    <Card className="h-100 w-100 text-center">
-
-                                        <Card.Body>
-                                            <img className="w-75" src="https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/catalog/product/cache/a4a71470c99ce7a4925275fbc94e38e5/a/m/amazfit-gts-4-mini-smart-watch-17-07-2022.jpg"/>
-                                            <h4 className="category-name">CATEGORY NAME</h4>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-
-                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                                    <Card className="h-100 w-100 text-center">
-
-                                        <Card.Body>
-                                            <img className="w-75" src="https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/catalog/product/cache/a4a71470c99ce7a4925275fbc94e38e5/a/m/amazfit-gts-4-mini-smart-watch-17-07-2022.jpg"/>
-                                            <h4 className="category-name">CATEGORY NAME</h4>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-
-                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                                    <Card className="h-100 w-100 text-center">
-
-                                        <Card.Body>
-                                            <img className="w-75" src="https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/catalog/product/cache/a4a71470c99ce7a4925275fbc94e38e5/a/m/amazfit-gts-4-mini-smart-watch-17-07-2022.jpg"/>
-                                            <h4 className="category-name">CATEGORY NAME</h4>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-
-
-                            </Row>
-                        </Col>
-
-                        <Col key={1} xl={6} lg={6} md={6} sm={12} xs={12}>
-                            <Row>
-                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                                    <Card className="h-100 w-100 text-center">
-
-                                        <Card.Body>
-                                            <img className="w-75" src="https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/catalog/product/cache/a4a71470c99ce7a4925275fbc94e38e5/a/m/amazfit-gts-4-mini-smart-watch-17-07-2022.jpg"/>
-                                            <h4 className="category-name">CATEGORY NAME</h4>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-
-                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                                    <Card className="h-100 w-100 text-center">
-
-                                        <Card.Body>
-                                            <img className="w-75" src="https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/catalog/product/cache/a4a71470c99ce7a4925275fbc94e38e5/a/m/amazfit-gts-4-mini-smart-watch-17-07-2022.jpg"/>
-                                            <h4 className="category-name">CATEGORY NAME</h4>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-
-                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                                    <Card className="h-100 w-100 text-center">
-
-                                        <Card.Body>
-                                            <img className="w-75" src="https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/catalog/product/cache/a4a71470c99ce7a4925275fbc94e38e5/a/m/amazfit-gts-4-mini-smart-watch-17-07-2022.jpg"/>
-                                            <h4 className="category-name">CATEGORY NAME</h4>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-
-                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                                    <Card className="h-100 w-100 text-center">
-
-                                        <Card.Body>
-                                            <img className="w-75" src="https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/catalog/product/cache/a4a71470c99ce7a4925275fbc94e38e5/a/m/amazfit-gts-4-mini-smart-watch-17-07-2022.jpg"/>
-                                            <h4 className="category-name">CATEGORY NAME</h4>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-
-                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                                    <Card className="h-100 w-100 text-center">
-
-                                        <Card.Body>
-                                            <img className="w-75" src="https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/catalog/product/cache/a4a71470c99ce7a4925275fbc94e38e5/a/m/amazfit-gts-4-mini-smart-watch-17-07-2022.jpg"/>
-                                            <h4 className="category-name">CATEGORY NAME</h4>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-
-                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                                    <Card className="h-100 w-100 text-center">
-
-                                        <Card.Body>
-                                            <img className="w-75" src="https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/catalog/product/cache/a4a71470c99ce7a4925275fbc94e38e5/a/m/amazfit-gts-4-mini-smart-watch-17-07-2022.jpg"/>
-                                            <h4 className="category-name">CATEGORY NAME</h4>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-
-                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                                    <Card className="h-100 w-100 text-center">
-
-                                        <Card.Body>
-                                            <img className="w-75" src="https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/catalog/product/cache/a4a71470c99ce7a4925275fbc94e38e5/a/m/amazfit-gts-4-mini-smart-watch-17-07-2022.jpg"/>
-                                            <h4 className="category-name">CATEGORY NAME</h4>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-
-                                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                                    <Card className="h-100 w-100 text-center">
-
-                                        <Card.Body>
-                                            <img className="w-75" src="https://azse77seaprodsa.blob.core.windows.net/b2b-dr-pickaboocdn/media/catalog/product/cache/a4a71470c99ce7a4925275fbc94e38e5/a/m/amazfit-gts-4-mini-smart-watch-17-07-2022.jpg"/>
-                                            <h4 className="category-name">CATEGORY NAME</h4>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-
-
-                            </Row>
-                        </Col>
-
+                        {/*<Col key={1} xl={6} lg={6} md={6} sm={12} xs={12}>*/}
+                        {/*    <Row>*/}
+                                {myView}
+                        {/*    </Row>*/}
+                        {/*</Col>*/}
 
                     </Row>
                 </Container>
